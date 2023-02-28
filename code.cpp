c06eb6c297c9b11892c8480d87cd63175fe314ec
Cho14f <hongcodau232@gmail.com>
2023-03-01 00:56:49 +0700

    Phien ban dau tien

---
+++ code.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+void sap_xep_day_so(int* a, int n)
+{
+    //ma lenh sap xep day so
+}
+int main
+{
+    cout <<"phien ban dau tien";
+    return 0;
+}
---
+++ code.h
@@ -0,0 +1,4 @@
+#include <iostream>
+
+using namespace std;
+void sap_xep_day_so(int* a, int n);
